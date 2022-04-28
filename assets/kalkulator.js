98d070307922084421af4880db00a491eef53bb1
muhammadharig <mharigunawansyah@gmail.com>
2022-04-28 16:24:39 +0700

    membuat function untuk menghapus

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -10,3 +10,11 @@ const calculator = {
 function updateDisplay() {
   document.querySelector("#displayNumber").innerHTML = calculator.displayNumber;
 }
+
+// membuat fungsi Clear Calculator
+function clearCalculator() {
+  calculator.displayNumber = "0";
+  calculator.operator = null;
+  calculator.firstNumber = null;
+  calculator.waitingForSecondNumber = false;
+}
